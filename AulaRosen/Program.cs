665f41e91097211a88c98984e028023d20e3f997
Carlos Eduardo <eduardofilho03@hotmail.com>
2025-08-12 19:51:14 -0300

    feat: else adicionado.

--- AulaRosen/Program.cs
+++ AulaRosen/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int idade = 23;
+Console.WriteLine($"Sua idade é: {idade}");
+
+if (idade >= 18)
+    Console.WriteLine("Você pode tirar habilitação!");
+else
+    Console.WriteLine("Você não pode tirar habilitação");
+
