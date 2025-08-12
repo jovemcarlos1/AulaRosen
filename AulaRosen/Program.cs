5bb032570eed7e56282e92028de641a6df493c0a
Carlos Eduardo <eduardofilho03@hotmail.com>
2025-08-12 20:37:55 -0300

    Feat: codigo alterado para entrada de nome e salário pelo usuário.

--- AulaRosen/Program.cs
+++ AulaRosen/Program.cs
@@ -1,9 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int idade = 23;
+/*int idade = 23;
 Console.WriteLine($"Sua idade é: {idade}");
 
 if (idade >= 18)
     Console.WriteLine("Você pode tirar habilitação!");
 else
-    Console.WriteLine("Você não pode tirar habilitação");
+    Console.WriteLine("Você não pode tirar habilitação");*/
 
+
+
+
+Console.WriteLine("Informe seu nome: ");
+string nome = Console.ReadLine();
+Console.WriteLine("informe seu salário: ");
+decimal salario = decimal.Parse(Console.ReadLine());
+Console.WriteLine($"Bem vindo(a) {nome}! O salário informado é de: R${salario} reais.");
