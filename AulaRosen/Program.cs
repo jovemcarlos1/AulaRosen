e46b1fb8d226f09ca81492de5f44e3ef621c68e9
Carlos Eduardo <eduardofilho03@hotmail.com>
2025-08-19 20:08:55 -0300

    bloco try catch

--- AulaRosen/Program.cs
+++ AulaRosen/Program.cs
@@ -7,26 +7,41 @@ if (idade >= 18)
 else
     Console.WriteLine("Você não pode tirar habilitação");*/
 
+try
+{
+    Console.WriteLine("Informe seu nome: ");
+    string nome = Console.ReadLine();
+    Console.WriteLine("informe seu salário: ");
+    decimal salario = decimal.Parse(Console.ReadLine());
+    Console.WriteLine($"Bem vindo(a) {nome}! O salário informado é de: R${salario} reais.");
 
-Console.WriteLine("Informe seu nome: ");
-string nome = Console.ReadLine();
-Console.WriteLine("informe seu salário: ");
-decimal salario = decimal.Parse(Console.ReadLine());
-Console.WriteLine($"Bem vindo(a) {nome}! O salário informado é de: R${salario} reais.");
+    //Informem o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto de IRPF
 
-//Informem o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto de IRPF
+    decimal irpf;
 
-decimal irpf;
+    if (salario <= 2259.20m)
+        irpf = 0;
+    else if (salario >= 2826.65m)
+        irpf = salario * 0.075m - 169.21m;
+    else if (salario <= 3751.05m)
+        irpf = salario * 0.15m - 381.44m;
+    else if (salario <= 4664.68m)
+        irpf = salario * 0.225m - 662.77m;
+    else
+        irpf = salario * 0.275m - 896m;
 
-if (salario <= 2259.20m)
-    irpf = 0;
-else if (salario >= 2826.65m)
-    irpf = salario * 0.075m - 169.21m;
-else if (salario <= 3751.05m)
-    irpf = salario * 0.15m - 381.44m;
-else if (salario <= 4664.68m)
-    irpf = salario * 0.225m - 662.77m;
-else
-    irpf = salario * 0.275m - 896m;
+    Console.WriteLine($"Para o salário de {salario} o valor a ser pago de IRPF é {irpf}, então o salário com desconto de IRPF será {salario - irpf}");
 
-Console.WriteLine($"Para o salário de {salario} o valor a ser pago de IRPF é {irpf}, então o salário com desconto de IRPF será {salario - irpf}");
+}
+catch(FormatException)
+{
+    Console.WriteLine("O salário deve ser informado em formato numérico.");
+}
+
+catch (Exception e)
+{
+    Console.WriteLine("Ocorreu um erro. Contate o Suporte Técnico.");
+    Console.WriteLine(e.StackTrace);
+}
+
+
