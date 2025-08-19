a965516e54629f5f1db21d6782c2af39eee58b1e
Carlos Eduardo <eduardofilho03@hotmail.com>
2025-08-19 19:36:52 -0300

    Exercício IRPF

--- AulaRosen/Program.cs
+++ AulaRosen/Program.cs
@@ -11,22 +11,22 @@ else
 Console.WriteLine("Informe seu nome: ");
 string nome = Console.ReadLine();
 Console.WriteLine("informe seu salário: ");
-double salario = double.Parse(Console.ReadLine());
+decimal salario = decimal.Parse(Console.ReadLine());
 Console.WriteLine($"Bem vindo(a) {nome}! O salário informado é de: R${salario} reais.");
 
 //Informem o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto de IRPF
 
 decimal irpf;
 
-if (salario < 2248.80)
-{
-    irpf = (decimal)salario * 0 - 0;
-    Console.WriteLine($"IRPF a ser pago: {irpf}");
+if (salario <= 2259.20m)
+    irpf = 0;
+else if (salario >= 2826.65m)
+    irpf = salario * 0.075m - 169.21m;
+else if (salario <= 3751.05m)
+    irpf = salario * 0.15m - 381.44m;
+else if (salario <= 4664.68m)
+    irpf = salario * 0.225m - 662.77m;
+else
+    irpf = salario * 0.275m - 896m;
 
-}
-else if (salario >= 2248.81 && salario <= 2826.65)
-{
-    irpf = (decimal)salario * 0.0075 - 182.16;
-    Console.WriteLine(irpf);
-
-}
+Console.WriteLine($"Para o salário de {salario} o valor a ser pago de IRPF é {irpf}, então o salário com desconto de IRPF será {salario - irpf}");
