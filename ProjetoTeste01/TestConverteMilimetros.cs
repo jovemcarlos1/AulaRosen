bc6aa573a803c08ce40235894edcb84b46482fd5
Carlos Eduardo <eduardofilho03@hotmail.com>
2025-09-02 20:43:15 -0300

    feat:Testes

---
+++ ProjetoTeste01/TestConverteMilimetros.cs
@@ -0,0 +1,26 @@
+﻿using AulaRosen.uteis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTeste01
+{
+    [TestClass]
+    internal class TestConverteMilimetros
+    {
+        [TestMethod]
+        public void Converter()
+        {
+            //cenário
+            int metro = 3;
+            
+            int resultado = MetrosMilimetros.Converter(metro);
+            
+            Assert.AreEqual(metro, resultado);
+            
+            
+        }
+    }
+}
